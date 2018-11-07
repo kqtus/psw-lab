b5b1199805c72dc59ab2119cb09a89bdd51a72e3
elMarrco <elmarcopl@gmail.com>
2018-11-07 02:12:32 +0100

    Zadania na lab z CSS2 i JS

---
+++ js/squares_anim.js
@@ -0,0 +1,67 @@
+var numSquares = 0;
+var squares = [];
+
+var max_width_str = '900';
+var max_height_str = '500';
+
+function createSquare(pos) {
+    numSquares++;
+
+    console.log('creating square on pos ' + pos)
+    var square = document.createElement("div");
+    square.style.left = pos.x;
+    square.style.right = pos.y;
+    square.style.width = "5px";
+    square.style.height = "5px";
+
+    var color = "";
+    switch (numSquares % 4) {
+        case 0:
+            color = "#000000";
+            break;
+        case 1:
+            color = "#111111";
+            break;
+        case 2:
+            color = "#222222";
+            break;
+        case 3:
+            color = "#333333";
+            break;
+    }
+
+    square.style.backgroundColor = color;
+    square.id = "square" + numSquares;
+    document.body.appendChild(square);
+
+    return square;
+}
+
+function getRandomPosOnScreen() {
+    var x = Math.floor(Math.random() * parseInt(max_width_str));
+    var y = Math.floor(Math.random() * parseInt(max_height_str));
+
+    return (x=x, y=y);
+}
+
+function createSquares() {
+    var num_squares_to_create = Math.floor(Math.random() * 50) + 50;
+
+    while (num_squares_to_create > 0) {
+        var square = createSquare(getRandomPosOnScreen());
+        squares.push(square);
+
+        num_squares_to_create--;
+    }
+}
+
+function updateSquare(square) {
+    square.style.left++;
+    square.style.right++;
+}
+
+function updateSquares() {
+    for (var i = 0; i < squares.length; i++) {
+        updateSquare(squares[i]);
+    }
+}
