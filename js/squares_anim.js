7e018597c79790c096218f7b1ded0cc524fd826f
elMarrco <elmarcopl@gmail.com>
2018-11-14 01:29:34 +0100

    JS continued

--- js/squares_anim.js
+++ js/squares_anim.js
@@ -1,41 +1,42 @@
 var numSquares = 0;
 var squares = [];
 
+var technologies = ['Redux', 'React', 'Qt', 'C++', 'Flask', 'Flutter', 'Spring', 'ASP.NET', 'NoSQL', 'Kotlin']
+
 var max_width_str = '900';
 var max_height_str = '500';
 
-var canvas;
+var squares_once_created = false;
 
 function createSquare(pos) {
     numSquares++;
 
     console.log('creating square on pos ' + pos)
     var square = document.createElement("div");
-    square.style.left = pos.x;
-    square.style.right = pos.y;
-    square.style.width = "5px";
-    square.style.height = "5px";
 
-    var color = "";
+    tech_str = technologies[Math.floor(Math.random() * technologies.length)]
+    square.textContent = tech_str;
+
+    var color_tint = "square-div ";
     switch (numSquares % 4) {
         case 0:
-            color = "#000000";
+            color_tint += "col1";
             break;
         case 1:
-            color = "#111111";
+            color_tint += "col2";
             break;
         case 2:
-            color = "#222222";
+            color_tint += "col3";
             break;
         case 3:
-            color = "#333333";
+            color_tint += "col4";
             break;
     }
 
-    square.style.backgroundColor = color;
+    square.className = color_tint;
     square.id = "square" + numSquares;
-    document.body.appendChild(square);
-
+    document.getElementsByClassName('tech-div')[0].appendChild(square);
+    
     return square;
 }
 
@@ -47,39 +48,24 @@ function getRandomPosOnScreen() {
 }
 
 function init() {
-    createCanvas();
+    var person = prompt("Please enter your name", "Harry Potter");
+    alert("You will be adding " + person);
     createSquares();
 }
 
-function createCanvas() {
-    canvas = document.createElement('canvas');
-
-    canvas.id = "Layer";
-    canvas.width = parseInt(max_width_str);
-    canvas.height = parseInt(max_height_str);
-    canvas.style.color = '#55FF55';
-
-    document.body.appendChild(canvas);
-}
 
 function createSquares() {
-    var num_squares_to_create = Math.floor(Math.random() * 50) + 50;
+    if (!squares_once_created) {
+        
+    }
+
+    squares_once_created = true;
+    var num_squares_to_create = 5; //Math.floor(Math.random() * 50) + 50;
 
     while (num_squares_to_create > 0) {
         var square = createSquare(getRandomPosOnScreen());
         squares.push(square);
 
         num_squares_to_create--;
-    }
-}
-
-function updateSquare(square) {
-    square.style.left++;
-    square.style.right++;
-}
-
-function updateSquares() {
-    for (var i = 0; i < squares.length; i++) {
-        updateSquare(squares[i]);
     }
 }
